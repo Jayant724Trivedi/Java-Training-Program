e6ae96b7b78eeb9b63486123d3651bf4dc056c1a
Jayant724Trivedi <63846569+Jayant724Trivedi@users.noreply.github.com>
2020-11-04 15:14:57 +0530

    Add files via upload

---
+++ WhileFactorialSum.java
@@ -0,0 +1,23 @@
+package com.ex;
+
+public class WhileFactorialSum {
+	public static void main(String args[])
+	{
+		int sum=0,fact=1,rem = 0,n=145;
+		while(n>0)
+		{
+			rem=n%10;
+		fact=1;
+		while(rem>0)
+		{
+			fact=fact*rem;
+			rem--;
+		}
+		sum=sum+fact;
+		n=n/10;
+	}
+	
+		System.out.println("sum:"+sum);
+}
+}
+
