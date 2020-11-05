a9342fd00e5ea0428b430016238e428316a0bdba
Jayant724Trivedi <63846569+Jayant724Trivedi@users.noreply.github.com>
2020-11-05 16:02:50 +0530

    Add files via upload

---
+++ Concat.java
@@ -0,0 +1,9 @@
+package com.ex;
+public class Concat {
+	public static void main(String args[])
+	{  
+	String s1="motiviylabs pvt limited";  
+	s1=s1.concat("\nhyderabad");  
+	System.out.println(s1);
+	}
+}
