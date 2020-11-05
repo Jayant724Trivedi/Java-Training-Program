2f6094841ffae81b64d03d3b073c069550690c6e
Jayant724Trivedi <63846569+Jayant724Trivedi@users.noreply.github.com>
2020-11-05 16:05:05 +0530

    Add files via upload

---
+++ FinallyBlock.java
@@ -0,0 +1,20 @@
+package com.ex;
+public class FinallyBlock {
+		  public static void main(String args[]){  
+		  try{  
+		   int data=125/25;  
+		  
+		  }  
+		  catch(ArithmeticException e)
+		  {
+			 e.printStackTrace();
+			  }  
+		  finally{
+			  
+			  	System.out.println("finally block is always executed");
+			  
+		  }  
+	}
+}  
+
+
