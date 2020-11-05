6899a710bfb208b4dcc515f24fc78aa765a0ab09
Jayant724Trivedi <63846569+Jayant724Trivedi@users.noreply.github.com>
2020-11-05 16:05:56 +0530

    Add files via upload

---
+++ NumberFormatException.java
@@ -0,0 +1,17 @@
+package com.ex;
+public class NumberFormatException {
+	public static void main (String args[])
+	  {  
+		  String s="bcd";
+		  int i=Integer.parseInt(s);
+	   try{   
+		
+		 System.out.print(i);
+	   }
+	   catch(Exception   e)
+	   {
+		   e.printStackTrace();
+		   }  
+	}
+
+}
