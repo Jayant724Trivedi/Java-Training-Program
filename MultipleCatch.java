b7b881566e24ce4f2b3c1113ea7aea207c1dbf82
Jayant724Trivedi <63846569+Jayant724Trivedi@users.noreply.github.com>
2020-11-05 16:05:31 +0530

    Add files via upload

---
+++ MultipleCatch.java
@@ -0,0 +1,17 @@
+package com.ex;
+public class MultipleCatch {
+		public static void main(String[] args) {
+			int a=10,b=0;
+			try {
+				String s=null;
+				try {
+					s.length();
+				}catch(NullPointerException np) {
+					np.printStackTrace();
+				}
+				int c=a/b;
+			}catch(ArithmeticException ae) {
+				ae.printStackTrace();
+			}
+		}
+}
