79aaa078f13636462077094255eff1d5aa665687
Jayant724Trivedi <63846569+Jayant724Trivedi@users.noreply.github.com>
2020-11-10 15:53:36 +0530

    Add files via upload

---
+++ CharArrayWriterExample.java
@@ -0,0 +1,16 @@
+package com.ex;
+import java.io.CharArrayWriter;  
+import java.io.FileWriter;  
+public class CharArrayWriterExample {  
+public static void main(String args[])throws Exception{    
+          CharArrayWriter caw=new CharArrayWriter();    
+          caw.write("Welcome to java methods");    
+          FileWriter f1=new FileWriter("E:\\a.txt");    
+          FileWriter f2=new FileWriter("E:\\b.txt");     
+          caw.writeTo(f1);    
+          caw.writeTo(f2);    
+          f1.close();    
+          f2.close();    
+          System.out.println("Success...");    
+         } 
+}
