6ba627c05d12d20370620d57f330f61a583b5dd4
Jayant724Trivedi <63846569+Jayant724Trivedi@users.noreply.github.com>
2020-11-10 15:52:17 +0530

    Add files via upload

---
+++ ReaderExample.java
@@ -0,0 +1,17 @@
+package com.ex;
+import java.io.*;  
+public class ReaderExample {  
+    public static void main(String[] args) {  
+        try {  
+            Reader reader = new FileReader("E:\\jnit.txt");  
+            int i = reader.read();  
+            while (i != -1) {  
+                System.out.print((char) i);  
+                i = reader.read();  
+            }  
+            reader.close();  
+        } catch (Exception e) {  
+            System.out.println(e.getMessage());  
+        }  
+    }  
+}  
