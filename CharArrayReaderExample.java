f9bde5a6e23ba1360aa0188fb41f27dcb9fe86d0
Jayant724Trivedi <63846569+Jayant724Trivedi@users.noreply.github.com>
2020-11-10 15:54:00 +0530

    Add files via upload

---
+++ CharArrayReaderExample.java
@@ -0,0 +1,14 @@
+package com.ex;
+import java.io.CharArrayReader;  
+public class CharArrayReaderExample{  
+  public static void main(String[] ag) throws Exception {  
+    char[] ary = { 'j', 'a', 'y', 'a', 'n', 't', 't', 'r', 'i', 'v','e','d','i' };  
+    CharArrayReader car = new CharArrayReader(ary);  
+    int i = 0;   
+    while ((i = car.read()) != -1) {  
+      char ch = (char) i;  
+      System.out.print(ch + " : ");  
+      System.out.println(i);  
+    }  
+  }  
+}  
