6eb62b2a4cda5fcac3570ffd17c24603adbcfe98
Jayant724Trivedi <63846569+Jayant724Trivedi@users.noreply.github.com>
2020-11-10 15:51:48 +0530

    Add files via upload

---
+++ WriterExample.java
@@ -0,0 +1,17 @@
+package com.ex;
+import java.io.*;
+
+public class WriterExample {
+		public static void main(String[] args) {  
+        try {  
+            Writer w = new FileWriter("E:\\jnit.txt");  
+            String content = "This is program of writer class:";  
+            w.write(content);  
+            w.close();  
+            System.out.println("Done");  
+        } catch (IOException e) {  
+            e.printStackTrace();  
+        }  
+    }  
+
+}
