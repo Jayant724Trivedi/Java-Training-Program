21efaee26d84de69cb34fc5bfa2ad6b90e76305a
Jayant724Trivedi <63846569+Jayant724Trivedi@users.noreply.github.com>
2020-11-17 18:46:32 +0530

    Add files via upload

---
+++ ThreadJoinEx.java
@@ -0,0 +1,26 @@
+package com.ex;
+	import java.util.Scanner;
+
+	class AliveEx extends Thread
+	{
+		int x,y;
+		public void run()
+		{
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter x,y values");
+			x=sc.nextInt();
+		    y=sc.nextInt();
+		}
+	}
+	public class ThreadJoinEx {
+
+		public static void main(String[] args) throws java.lang.InterruptedException {
+			AliveEx a=new AliveEx();
+			a.start();
+			a.join();
+			System.out.println("Sum ="+(a.x+a.y));
+			
+		}
+
+
+}
